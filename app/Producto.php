96a799ded998ddb0e04dc4352bda246141e9b1e9
Nilda99 <52302684+Nilda99@users.noreply.github.com>
2020-03-10 23:34:13 -0400

    creando modelo productos

--- app/Producto.php
+++ app/Producto.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
-    //
+    protected $table = 'productos';
+    protected $primaryKey = 'idProducto';
+    protected $fillable = [
+        'nombre',
+        'precio',
+        'cantidad',
+        'idSubCategoria'
+
+    ];
 }
