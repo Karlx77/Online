c0daf04fa0a6e69623ac0fd71a8c14737ad31c58
Nilda99 <52302684+Nilda99@users.noreply.github.com>
2020-03-10 23:24:35 -0400

    creando modelo categoria

--- app/Categoria.php
+++ app/Categoria.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    //
+    protected $table = 'categorias'; // nombre de la tabla de la db
+    protected $primaryKey = 'idCategoria'; // de la db
+
+    protected $fillable = [ // para los atributos de la tabla
+        'nombre' ,
+    ];
 }
