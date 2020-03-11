6aecb8a76bc3259fdf03e9fc3c789ad16f5583a8
Nilda99 <52302684+Nilda99@users.noreply.github.com>
2020-03-10 23:55:05 -0400

    creando modelo ciudads

--- app/Ciudad.php
+++ app/Ciudad.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ciudad extends Model
 {
-    //
+    protected $table='ciudads';
+    protected $primaryKey='idCiudad';
+    protected $fillable = [
+        'nombre',
+        'precio',
+        'idUsuarioAdmin'
+        ];
 }
