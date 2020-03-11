94bad39fb0e84491f80cc9391684067a5451d518
Nilda99 <52302684+Nilda99@users.noreply.github.com>
2020-03-10 23:41:46 -0400

    creando modelo detalle_pedido

--- app/DetallePedido.php
+++ app/DetallePedido.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Detalle_pedido extends Model
 {
-    //
+    protected $table='detalle_pedidos';
+    protected $primaryKey='idDetallePedido';
+    protected $fillable = [
+        'cantidad',
+        'subTotal',
+        'idProducto',
+        'idPedido'
+    ];
 }
